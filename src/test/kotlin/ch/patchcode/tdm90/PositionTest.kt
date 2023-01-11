88342badfbeaaeec54386adcd51ed4eb8a76d8ae
Christian Fuchs <kontaktierbar@gmail.com>
2023-01-11 22:05:31 +0100

    implementing tileName - add case

--- src/test/kotlin/ch/patchcode/tdm90/PositionTest.kt
+++ src/test/kotlin/ch/patchcode/tdm90/PositionTest.kt
@@ -13,10 +13,16 @@ class PositionTest {
         assertEquals("TDM1_DEM__30_N00E000", geoTiffName)
     }
 
-
     @Test
     fun tileNameN05E000() {
         val position = Position(longitude = 0.0, latitude = 5.0)
+        val geoTiffName = position.tileName()
+        assertEquals("TDM1_DEM__30_N05E000", geoTiffName)
+    }
+
+    @Test
+    fun tileNameN05E000_slightlyNorth() {
+        val position = Position(longitude = 0.0, latitude = 5.1)
         val geoTiffName = position.tileName()
         assertEquals("TDM1_DEM__30_N05E000", geoTiffName)
     }
